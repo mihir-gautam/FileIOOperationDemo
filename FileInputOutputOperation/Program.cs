ee91e22752e6860714a3c0fe3313fb185c459313
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-10-26 20:09:11 +0530

    [Mihir] Add. File Input Output Learning problem

---
+++ FileInputOutputOperation/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+
+namespace FileInputOutputOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the File Input Output program in C#");
+            Console.WriteLine("\n=====================================================================================");
+            FileExists();
+            Console.WriteLine("\n=====================================================================================");
+            ReadAllLines(); 
+            Console.WriteLine("\n=====================================================================================");
+            ReadAllText();
+            Console.WriteLine("\n=====================================================================================");
+            FileCopy();
+            Console.WriteLine("\n=====================================================================================");
+            DeleteFile();
+            Console.WriteLine("\n=====================================================================================");
+            ReadFromStreamReader();
+            Console.WriteLine("\n=====================================================================================");
+            WriteUsingStreamWriter();
+            Console.WriteLine("\n=====================================================================================");
+            
+            SerializationAndDeserialisation SD = new SerializationAndDeserialisation();
+            SD.BinarySerialization();
+            Console.WriteLine("\n=====================================================================================");
+            SD.BinaryDeSerialization();
+            Console.WriteLine("\n=====================================================================================");
+        }
+        public static void FileExists()
+        {
+            string path = @"C:\Users\Mihir Gautam\source\repos\FileInputOutputOperation\FileInputOutputOperation\Sample.txt";
+            if (File.Exists(path))
+            {
+                Console.WriteLine("File Exists");
+            }
+        }
+        public static void ReadAllLines()
+        {
+            string path = @"C:\Users\Mihir Gautam\source\repos\FileInputOutputOperation\FileInputOutputOperation\Sample.txt";
+            string[] lines;
+            lines = File.ReadAllLines(path);
+            Console.WriteLine("Read All Lines");
+            Console.WriteLine(lines[0]);
+            Console.WriteLine(lines[1]);
+        }
+        public static void ReadAllText()
+        {
+            string path = @"C:\Users\Mihir Gautam\source\repos\FileInputOutputOperation\FileInputOutputOperation\Sample.txt";
+            string lines;
+            lines = File.ReadAllText(path);
+            Console.WriteLine("Read All Text");
+            Console.WriteLine(lines);
+        }
+        public static void FileCopy()
+        {
+            string path = @"C:\Users\Mihir Gautam\source\repos\FileInputOutputOperation\FileInputOutputOperation\Sample.txt";
+            string copypath = @"C:\Users\Mihir Gautam\source\repos\FileInputOutputOperation\FileInputOutputOperation\SampleCopy.txt";
+
+            File.Copy(path, copypath);
+            Console.WriteLine("File successfuly copied");
+        }
+        public static void DeleteFile()
+        {
+            string path = @"C:\Users\Mihir Gautam\source\repos\FileInputOutputOperation\FileInputOutputOperation\SampleCopy.txt";
+            File.Delete(path);
+            Console.WriteLine("file successfully deleted");
+        }
+        public static void ReadFromStreamReader()
+        {
+            Console.WriteLine("Read from stream reader");
+            string path = @"C:\Users\Mihir Gautam\source\repos\FileInputOutputOperation\FileInputOutputOperation\Sample.txt";
+            using(StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine())!=null)
+                    {
+                    Console.WriteLine(s);
+                }
+            
+            }
+            
+        }
+        public static void WriteUsingStreamWriter()
+        {
+            Console.WriteLine("Write using stream writer");
+            string path = @"C:\Users\Mihir Gautam\source\repos\FileInputOutputOperation\FileInputOutputOperation\Sample.txt";
+             using (StreamWriter sr = File.AppendText(path))
+            {
+                sr.WriteLine(" Welcome all - Have a good day");
+                sr.Close();
+
+                Console.WriteLine(File.ReadAllText(path));
+            }
+        }
+    }
+}
---
+++ FileInputOutputOperation/SerializationAndDeserialisation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace FileInputOutputOperation
+{
+    public class SerializationAndDeserialisation
+    {
+        public string path = "abc";
+
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+
+        public void BinarySerialization()
+        {
+            Demo demo = new Demo();
+            FileStream fileStream = new FileStream(path, FileMode.Create);
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            binaryFormatter.Serialize(fileStream, demo);
+            Console.ReadKey();
+        }
+        public void BinaryDeSerialization()
+        {
+            Demo demo = new Demo();
+            FileStream fileStream = new FileStream(path, FileMode.Create);
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            binaryFormatter.Deserialize(fileStream);
+            Console.WriteLine("Application Id:{0} ", demo.ApplicationId);
+            Console.WriteLine("Application Name:{0} ", demo.ApplicationName);
+            Console.ReadKey();
+        }
+        [Serializable]
+        public class Demo
+        {
+            public string ApplicationName { get; set; }
+            public int ApplicationId { get; set; } = 1001;
+        }
+
+
+    }
+}
